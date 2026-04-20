0b61214cede23df242665e287bfa76cf518e64da
celinekuttler <194842828+celinekuttler@users.noreply.github.com>
2026-04-20 11:23:44 +0000

    add dodumentation in files models/{docs.md,overview.md}

---
+++ airbnb/models/fct/fct_reviews.sql
@@ -0,0 +1,19 @@
+{{
+    config(
+        materialized = 'incremental',
+        on_schema_change='fail'
+    )
+}}
+WITH src_reviews AS (
+    SELECT * FROM {{ ref('src_reviews') }}
+)
+SELECT 
+{{ dbt_utils.generate_surrogate_key(['listing_id','review_id','reviewer_name','review_text']) }} as review_id,
+*
+FROM src_reviews 
+WHERE review_text is not null
+{% if is_incremental() %}
+AND review_date > (
+    SELECT MAX(review_date) FROM {{ this }}
+)
+{% endif %}
---
+++ airbnb/models/overview.md
@@ -0,0 +1,8 @@
+{% docs __overview__ %}
+
+Welcome to our Airbnb pipeline doc!
+
+Here is the schema of our input data:
+![input schema](assets/input_schema.png)
+
+{% enddocs %}
